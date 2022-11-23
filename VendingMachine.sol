4f046a844b9ee66122cf7b3ff1bdff1ef57de802
Mohammed Rizwan <112799398+mohammedrizwann123@users.noreply.github.com>
2022-11-23 22:58:10 +0530

    Add files via upload

---
+++ VendingMachine.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier:MIT
+// Contract created by Mohammed Rizwan
+
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract VendingMachine {
+    address payable public owner;
+    mapping(address=>uint) public donutBalances;
+
+    constructor(){
+        owner = payable (msg.sender);
+        donutBalances[address(this)] = 100;
+    }
+
+    modifier onlyOwner(){
+        require(owner == msg.sender, "Only owner can access this vending machine function.");
+        _;
+    }
+
+    event amountReceived(address _buyerAddress, uint _amountPaid);
+
+    receive() external payable {
+        emit amountReceived(msg.sender, msg.value);
+    }
+
+    function getVMBalance() public view returns(uint) {
+        return donutBalances[address(this)];
+    }
+
+    function checkContractBalance() public view onlyOwner returns(uint) {
+        return address(this).balance;
+    }
+
+    function restock(uint _addDonuts) public onlyOwner {
+        donutBalances[address(this)] += _addDonuts;
+    }
+
+    function purchase(uint _quantity) public payable {
+        require(msg.value == _quantity * 1 ether, "You must pay atleast 1 ether per donut");
+        require (donutBalances[address(this)] >= _quantity, "Donut is not in stock");
+        donutBalances[address(this)] -= _quantity;
+        donutBalances[msg.sender] += _quantity;
+    }
+
+    function withdraw() public onlyOwner {
+        owner.transfer(address(this).balance);
+    }
+
+}
